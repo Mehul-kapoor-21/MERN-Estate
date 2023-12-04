de6adec560ed0f1e72c671a76109a4ef232f95d8
Mehul-kapoor-21 <mehulkapoor308@gmail.com>
2023-12-04 15:36:19 +0530

    Intial Commit

---
+++ api/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
+
+const authRoute = express.Router();
+
+authRoute.post("/signup", signup);
+
+export default authRoute;
+//pp
