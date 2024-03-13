494e45704df0bf964ac696744a2ac7ef88b29d54
Mehul-kapoor-21 <mehulkapoor308@gmail.com>
2024-03-13 11:36:35 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing,deleteListing } from "../controllers/listing.controller.js";
+import { createListing,deleteListing,updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 
@@ -7,5 +7,6 @@ const listingrouter = express.Router();
 
 listingrouter.post("/create",verifyToken, createListing);
 listingrouter.delete('/delete/:id',verifyToken,deleteListing);
+listingrouter.post('/update/:id',verifyToken,updateListing);
 
 export default listingrouter;
